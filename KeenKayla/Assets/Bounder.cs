b2205cfc6c5649eb66285bef395448806e02d25e
Matt Bitner <matt.bitner@gmail.com>
2015-12-17 07:46:53 -0600

    spikes

--- KeenKayla/Assets/Bounder.cs
+++ KeenKayla/Assets/Bounder.cs
@@ -3,36 +3,54 @@ using System.Collections;
 
 public class Bounder : Enemy
 {
-    public float wanderRange = 10;
-    public float speed = 0.5f;
+    public float speedX = 6f;
+    public float speedY = 5f;
 
-    private Vector3 _startingPosition;
-    private Vector3 _targetPosition;
-    private Vector3 _direction = Vector3.right;
+    private float _directionX;
     private Quaternion _flippedFacing = Quaternion.Euler(0, 180, 0);
+    private float _timer;
+    private float _time = 1.5f;
+    private float _startingY;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _startingY = transform.position.y;
+    }
 
     protected override void Update()
     {
         base.Update();
-    
-        if (transform.position != _targetPosition)
+
+        if(_timer < _time)
         {
-            transform.position = Vector3.MoveTowards(transform.position, _targetPosition, speed * Time.deltaTime);
+            
+            _timer += Time.deltaTime;
+            transform.position += _directionX * Vector3.right * speedX * Time.deltaTime;
+            if(_timer < _time*0.5f)
+            {
+                var delta= (_time*0.5f - _timer) / (_time*0.5f);
+                transform.position += Vector3.up * delta * speedY * Time.deltaTime;
+            }
+            else
+            {
+                var delta = (_timer -_time*0.5f) / (_time * 0.5f);
+                transform.position += Vector3.down * delta * speedY * Time.deltaTime;
+            }
         }
         else
         {
-            _direction *= -1;
-            _targetPosition = _startingPosition + wanderRange * _direction;
-
-            if (_direction.x < 0 && transform.rotation != _flippedFacing)
-            {
-                transform.rotation = _flippedFacing;
-            }
-            else if (_direction.x > 0 && transform.rotation != Quaternion.identity)
-            {
-                transform.rotation = Quaternion.identity;
-            }
+            SetupJump();
         }
+    }
+
+    public void SetupJump()
+    {
+        var position = transform.position;
+        position.y = _startingY;
+        transform.position = position;
+        _timer = 0;
+        _directionX = Random.Range((int)-1, (int)1);
     }
 
     public override void Die()
---
+++ KeenKayla/Assets/MovingPlatform.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour
+{
+    private Vector3 _lastPosition;
+    private bool _playerPresent;
+
+    public void LateUpdate()
+    {
+        if(_playerPresent)
+        {
+            PlayerController.instance.transform.position += transform.position - _lastPosition;
+        }
+
+        _lastPosition = transform.position;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        _playerPresent = true;
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        _playerPresent = false;
+    }
+}

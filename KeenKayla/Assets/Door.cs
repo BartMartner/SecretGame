429f1595e35b38e161e8e276411264a6d3d9c84d
Matt Bitner <matt.bitner@gmail.com>
2016-01-02 14:21:16 -0600

    a door

---
+++ KeenKayla/Assets/Door.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : Damagable
+{
+    public BoxCollider2D boxCollider2D;
+    public GameObject shield;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        boxCollider2D = GetComponentInChildren<BoxCollider2D>();
+    }
+
+    public override bool Hurt(float damage, GameObject source = null)
+    {        
+        boxCollider2D.enabled = false;
+        shield.gameObject.SetActive(false);
+        return true;
+    }
+}

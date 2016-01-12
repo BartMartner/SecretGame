bbabe93550d7093ef7737530420327d25f76c25c
Matt Bitner <matt.bitner@gmail.com>
2016-01-11 19:30:40 -0600

    health bar

--- KeenKayla/Assets/Scripts/UI/HealthBar.cs
+++ KeenKayla/Assets/Scripts/UI/HealthBar.cs
@@ -4,22 +4,61 @@ using System.Collections;
 
 public class HealthBar : MonoBehaviour 
 {
-    public Image innerBar;
+    public Sprite heart;
+    public Sprite halfHeart;
+    public Sprite empty;
+    private Image[] hearts;
     private float _lastValue;
-	
+    private float _lastMaxValue;
+
     private void Awake()
     {
-        innerBar.type = Image.Type.Filled;
-        innerBar.fillMethod = Image.FillMethod.Horizontal;
+        hearts = GetComponentsInChildren<Image>();
+    }
+
+    private void Start()
+    {
+        SetHearts();
     }
 
 	// Update is called once per frame
 	private void Update () 
     {
-        if (_lastValue != Player.instance.health)
+        if (_lastValue != Player.instance.health || _lastMaxValue != Player.instance.maxHealth)
         {
             _lastValue = Player.instance.health;
-            innerBar.fillAmount = Player.instance.healthRatio;
+            _lastMaxValue = Player.instance.maxHealth;
+            SetHearts();
         }
 	}
+
+    private void SetHearts()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if(i > Player.instance.maxHealth - 1)
+            {
+                hearts[i].gameObject.SetActive(false);
+            }
+            else
+            {
+                hearts[i].gameObject.SetActive(true);
+                if(i+1 <= Player.instance.health)
+                {
+                    hearts[i].sprite = heart;
+                }
+                else
+                {
+                    if(i <= Player.instance.health && Player.instance.health % 1 > 0.45f)
+                    {
+                        hearts[i].sprite = halfHeart;
+                    }
+                    else
+                    {
+                        hearts[i].sprite = empty;
+                    }
+                }
+            }
+        }
+    }
 }

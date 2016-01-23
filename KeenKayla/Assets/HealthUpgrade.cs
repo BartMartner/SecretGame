a2317051e3bc359771fa3a7140c22eb51fdce26a
Matt Bitner <matt.bitner@hitcents.com>
2016-01-22 22:00:39 -0600

    more stuff

---
+++ KeenKayla/Assets/HealthUpgrade.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthUpgrade : Pickup
+{
+    public int id;
+
+    public override void OnPickup()
+    {
+        SaveGameManager.instance.saveGameData.healthUpgradesCollected.Add(id);
+        Player.instance.maxHealth += 1;
+        Player.instance.health = Player.instance.maxHealth;
+        SaveGameManager.instance.SaveGame();
+        base.OnPickup();
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawIcon(transform.position, id.ToString());
+    }
+}

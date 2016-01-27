f230e7e4a18af51390ba2adec4ae3b7a89d55d36
Matt Bitner <matt.bitner@gmail.com>
2016-01-27 12:07:16 -0600

    more grayboxing

--- KeenKayla/Assets/Scripts/Bomb.cs
+++ KeenKayla/Assets/Scripts/Bomb.cs
@@ -5,6 +5,6 @@ public class Bomb : MonoBehaviour
 {
     public void OnDestroy()
     {
-        ProjectileManager.instance.SpawnExplosion(transform.position);
+        ProjectileManager.instance.SpawnExplosion(transform.position + Vector3.up * 0.125f);
     }
 }

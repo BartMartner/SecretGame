68d24b93b9906b567aa3f912ca12b3266cd2d62c
Matt Bitner <matt.bitner@gmail.com>
2016-02-02 15:26:43 -0600

    lazer eye animations

--- KeenKayla/Assets/Scripts/Enemies/LazerEye.cs
+++ KeenKayla/Assets/Scripts/Enemies/LazerEye.cs
@@ -35,10 +35,17 @@ public class LazerEye : Enemy
     private IEnumerator FireLazer()
     {
         _firingLazer = true;
-        yield return new WaitForSeconds(2);
-        yield return StartCoroutine(Flash(8, 0.1f, Color.yellow, 0.5f));
-        lazer.SetActive(true);
         yield return new WaitForSeconds(3);
+        animator.SetTrigger("Charge");
+        yield return StartCoroutine(Flash(1, 0.1f, new Color32(252,84,252,255), 0.1f));
+        yield return StartCoroutine(Flash(1, 0.1f, new Color32(252, 84, 252, 255), 0.2f));
+        yield return StartCoroutine(Flash(1, 0.1f, new Color32(252, 84, 252, 255), 0.3f));
+        yield return StartCoroutine(Flash(1, 0.1f, new Color32(252, 84, 252, 255), 0.4f));
+        yield return StartCoroutine(Flash(1, 0.1f, new Color32(252, 84, 252, 255), 0.5f));
+        lazer.SetActive(true);
+        animator.SetBool("Firing", true);
+        yield return StartCoroutine(Flash(15, 0.05f, new Color32(252, 84, 252, 255), 0.25f));
+        animator.SetBool("Firing", false);
         lazer.SetActive(false);
         _firingLazer = false;
     }
